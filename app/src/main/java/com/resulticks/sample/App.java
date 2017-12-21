edf17c49084e4c8d22e227605302d38e9dc6742d
resulticks <33717153+resulticks@users.noreply.github.com>
2017-12-21 12:27:30 +0530

    issue fixes

--- app/src/main/java/com/resulticks/sample/App.java
+++ app/src/main/java/com/resulticks/sample/App.java
@@ -1,6 +1,7 @@
 package com.resulticks.sample;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import io.mob.resu.reandroidsdk.ReAndroidSDK;
 
@@ -14,6 +15,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().build());
+
         ReAndroidSDK.getInstance(this);
     }
 }
